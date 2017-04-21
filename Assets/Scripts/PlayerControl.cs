afeea396a208d515481b5456e5fa71711cc93bf8
EmilyClaudet <emily@kano.me>
2017-04-22 01:31:31 +0200

    Updated animations

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -13,8 +13,9 @@ public class PlayerControl : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		DarkRealm = false;
-		DarkRealmImage = GameObject.Find("Dark Realm").GetComponent<Image>();
-		playerAnimator = GetComponent<Animator> ();
+		// DarkRealmImage = GameObject.Find("Dark Realm").GetComponent<Image>();
+		playerAnimator = GetComponent<Animator>();
+		playerAnimator.enabled = true;
 	}
 	
 	// Update is called once per frame
@@ -23,22 +24,39 @@ public class PlayerControl : MonoBehaviour {
 			transform.position += new Vector3(0, -playerSpeed*Time.deltaTime);
 			playerAnimator.Play("walkDown");
 		}
+	    if (Input.GetKeyUp("down")) {
+			playerAnimator.Play("Idle");
+		}
+	    if (Input.GetKeyUp("up")) {
+			playerAnimator.Play("IdleDown");
+		}
+	    if (Input.GetKeyUp("right")) {
+			playerAnimator.Play("IdleRight");
+		}
+	    if (Input.GetKeyUp("left")) {
+			playerAnimator.Play("IdleLeft");
+		}		
+
 		if (Input.GetKey("up")) {
 			transform.position += new Vector3(0, playerSpeed*Time.deltaTime);
 			playerAnimator.Play("walkUp");
 		}
+
 		if (Input.GetKey("right")) {
 			transform.position += new Vector3(playerSpeed*Time.deltaTime, 0);
 			playerAnimator.Play("walkRight");
 		}
+
 		if (Input.GetKey("left")) {
 			transform.position += new Vector3(-playerSpeed*Time.deltaTime, 0);
 			playerAnimator.Play("walkLeft");
 		}
+
+		
 		if (Input.GetKey("tab")) {
 			Debug.Log("You are entering the Dark Realm");
 			ChangeWorlds();
-		}		
+		}
 	}
 
 void ChangeWorlds() {
@@ -46,12 +64,12 @@ void ChangeWorlds() {
 	if (DarkRealm == false) {
 		//Enable UI Panel Dark Realm to be true
 		DarkRealm = true;
-		DarkRealmImage.enabled = true;
+		// DarkRealmImage.enabled = true;
 	}
 	if (DarkRealm == true) {
 		// ignore input
 		DarkRealm = false;
-		DarkRealmImage.enabled = false;
+		// DarkRealmImage.enabled = false;
 	}
 }
 
