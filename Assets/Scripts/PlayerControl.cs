8883b3258cfa6a43c429db09b8aafe6ce4221aa9
EmilyClaudet <emily@kano.me>
2017-04-21 23:40:08 +0200

    Dark Realm UI

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerMove : MonoBehaviour {
+public class PlayerControl : MonoBehaviour {
 
 	public float playerSpeed;
+	private bool DarkRealm; 
+	private Image DarkRealmImage;
+
 	// Use this for initialization
 	void Start () {
-		
+		DarkRealm = false;
+		DarkRealmImage = GameObject.Find("Dark Realm").GetComponent<Image>();
 	}
 	
 	// Update is called once per frame
@@ -23,6 +28,25 @@ public class PlayerMove : MonoBehaviour {
 		}
 		if (Input.GetKey("left")) {
 			transform.position += new Vector3(-playerSpeed*Time.deltaTime, 0);
+		}
+		if (Input.GetKey("tab")) {
+			Debug.Log("You are entering the Dark Realm");
+			ChangeWorlds();
 		}		
 	}
+
+void ChangeWorlds() {
+	// Code to change back and forth between Reality and Dark Realm
+	if (DarkRealm == false) {
+		//Enable UI Panel Dark Realm to be true
+		DarkRealm = true;
+		DarkRealmImage.enabled = true;
+	}
+	if (DarkRealm == true) {
+		// ignore input
+		DarkRealm = false;
+		DarkRealmImage.enabled = false;
+	}
+}
+
 }
