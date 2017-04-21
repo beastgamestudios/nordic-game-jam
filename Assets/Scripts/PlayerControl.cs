a40dafc89fcf91dd289058edb697f3c4029d484c
EmilyClaudet <emily@kano.me>
2017-04-22 00:02:35 +0200

    Player animations

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class PlayerControl : MonoBehaviour {
 
 	public float playerSpeed;
+	private Animator playerAnimator;
 	private bool DarkRealm; 
 	private Image DarkRealmImage;
 
@@ -13,21 +14,26 @@ public class PlayerControl : MonoBehaviour {
 	void Start () {
 		DarkRealm = false;
 		DarkRealmImage = GameObject.Find("Dark Realm").GetComponent<Image>();
+		playerAnimator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey("down")) {
 			transform.position += new Vector3(0, -playerSpeed*Time.deltaTime);
+			playerAnimator.Play("walkDown");
 		}
 		if (Input.GetKey("up")) {
 			transform.position += new Vector3(0, playerSpeed*Time.deltaTime);
+			playerAnimator.Play("walkUp");
 		}
 		if (Input.GetKey("right")) {
 			transform.position += new Vector3(playerSpeed*Time.deltaTime, 0);
+			playerAnimator.Play("walkRight");
 		}
 		if (Input.GetKey("left")) {
 			transform.position += new Vector3(-playerSpeed*Time.deltaTime, 0);
+			playerAnimator.Play("walkLeft");
 		}
 		if (Input.GetKey("tab")) {
 			Debug.Log("You are entering the Dark Realm");
