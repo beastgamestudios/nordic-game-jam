6387deeb26f6ffbf79c610f0a74e3b308e97923f
Catherine Claudet <cmclaudet@gmail.com>
2017-04-21 23:41:40 +0200

    Added Object detection on ghost script

---
+++ Assets/Scripts/detectObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detectObject : MonoBehaviour {
+	public Transform objectPrefab;
+	public Transform possessedObjectPrefab;
+	private string objectTag;
+
+	// Use this for initialization
+	void Start () {
+		objectTag = objectPrefab.gameObject.tag;
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == objectTag) {
+			Instantiate(possessedObjectPrefab);
+			gameObject.SetActive(false);
+		}
+	}
+}
