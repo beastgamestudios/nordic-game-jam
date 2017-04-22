57dc840bc089f73c7e9554e892da4a92621bc4d5
EmilyClaudet <emily@kano.me>
2017-04-22 23:41:56 +0200

    Added dialogue box

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -22,7 +22,7 @@ public class PlayerHealth : MonoBehaviour {
 	}
 
 	public void ReduceHealth() {
-		playerHealth -= 1;
+		playerHealth -= Time.deltaTime;
         Debug.Log("Player Health:" + playerHealth);		
 	}
 
