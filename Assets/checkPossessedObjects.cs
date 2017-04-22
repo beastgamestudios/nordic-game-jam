576a4c5dba993d8bb2c17e46375d3c6538939e0c
Catherine Claudet <cmclaudet@gmail.com>
2017-04-22 21:20:20 +0200

    Added logic for detecting ghost on strike

---
+++ Assets/checkPossessedObjects.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkPossessedObjects : MonoBehaviour {
+	private GameObject ghostInCollider;
+	private enum directions{UP, RIGHT, DOWN, LEFT};
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.CompareTag("possessedObject")) {
+			ghostInCollider = other.gameObject;
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D other) {
+		if (other.gameObject == ghostInCollider) {
+			ghostInCollider = null;
+		}
+	}
+
+	public void checkGhostInRange(int direction) {
+		if (direction == transform.GetSiblingIndex() && ghostInCollider != null) {
+			Debug.Log("kill ghost");
+		}
+	}
+
+}
