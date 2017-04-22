df48096efcacc7f8e80e5554e72030f6e18956e5
Catherine Claudet <cmclaudet@gmail.com>
2017-04-22 10:16:20 +0200

    Added animation prefix script

---
+++ Assets/Scripts/animationPrefixes.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationPrefixes : MonoBehaviour {
+	public Dictionary<string, Dictionary<string, string>> allAnimations;
+	
+	private string[] keyNames = new string[] {"idle", "walk", "holdIdle", "walkHold"};
+	private string[] upNames = new string[] {"upIdle", "walkUp", "IdleUpHold", "walkUpHold"};
+	private string[] rightNames = new string[] {"rightIdle", "walkRight", "IdleRightHold", "walkRightHold"};
+	private string[] downNames = new string[] {"downIdle", "walkDown", "IdleDownHold", "walkDownHold"};
+	private string[] leftNames = new string[] {"leftIdle", "walkLeft", "IdleLeftHold", "walkLeftHold"};
+
+	// Use this for initialization
+	void Start () {
+		Dictionary <string, string> up = defineAnimation(upNames);
+		Dictionary <string, string> right = defineAnimation(rightNames);
+		Dictionary <string, string> down = defineAnimation(downNames);
+		Dictionary <string, string> left = defineAnimation(leftNames);
+
+		allAnimations = new Dictionary<string, Dictionary<string, string>> {
+			{"up", up},
+			{"right", right},
+			{"down", down},
+			{"left", left}
+		};
+	}
+
+	private Dictionary<string, string> defineAnimation(string[] animationNames) {
+		return new Dictionary<string, string> {
+			{keyNames[0], animationNames[0]},
+			{keyNames[1], animationNames[1]},
+			{keyNames[2], animationNames[2]},
+			{keyNames[3], animationNames[3]}
+		};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
