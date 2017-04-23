20e9d86b5921145ac556feb7b297a6a40480e922
Catherine Claudet <cmclaudet@gmail.com>
2017-04-23 04:06:41 +0200

    Fixed animation bug

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -38,7 +38,7 @@ public class PlayerControl : MonoBehaviour {
 		ghostSprite = Ghost.GetComponent<SpriteRenderer>() as SpriteRenderer;
 		playerAnimator = GetComponent<Animator>();
 		playerAnimator.enabled = true;
-		allAnimations = GetComponent<animationPrefixes>().allAnimations;
+		setAnimationNames();
 		currentDirectionAnimations = allAnimations[(int)directions.DOWN];
 	}
 	
@@ -112,6 +112,20 @@ public class PlayerControl : MonoBehaviour {
 	}
 
 }
+
+void setAnimationNames () {
+		string[] upNames = new string[] {"IdleUp", "walkUp", "IdleUpHold", "walkUpHold", "AttackUp", "HurtUp"};
+		string[] rightNames = new string[] {"IdleRight", "walkRight", "IdleRightHold", "walkRightHold", "AttackRight", "HurtRight"};
+		string[] downNames = new string[] {"IdleDown", "walkDown", "IdleDownHold", "walkDownHold", "AttackDown", "HurtDown"};
+		string[] leftNames = new string[] {"IdleLeft", "walkLeft", "IdleLeftHold", "walkLeftHold", "AttackLeft", "HurtLeft"};
+
+		allAnimations = new string[][] {
+			upNames,
+			rightNames,
+			downNames,
+			leftNames
+		};
+	}
 
 void playIdleAnim() {
 	if (isHolding) {
---
+++ Assets/Scripts/animationData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationData : MonoBehaviour {
+
+	[HideInInspector]public string[][] allAnimations;
+	
+	private string[] upNames;
+	private string[] rightNames;
+	private string[] downNames;
+	private string[] leftNames;
+
+	// Use this for initialization
+	void Start () {
+		upNames = new string[] {"IdleUp", "walkUp", "IdleUpHold", "walkUpHold", "AttackUp", "HurtUp"};
+		rightNames = new string[] {"IdleRight", "walkRight", "IdleRightHold", "walkRightHold", "AttackRight", "HurtRight"};
+		downNames = new string[] {"IdleDown", "walkDown", "IdleDownHold", "walkDownHold", "AttackDown", "HurtDown"};
+		leftNames = new string[] {"IdleLeft", "walkLeft", "IdleLeftHold", "walkLeftHold", "AttackLeft", "HurtLeft"};
+
+		allAnimations = new string[][] {
+			upNames,
+			rightNames,
+			downNames,
+			leftNames
+		};
+	}
+}
