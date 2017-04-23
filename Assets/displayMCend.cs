d3fdb936537d46c6ec09f8eea6cd7a78a61cf2f4
Catherine Claudet <cmclaudet@gmail.com>
2017-04-23 11:56:48 +0200

    MC speaks at the end

---
+++ Assets/displayMCend.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class displayMCend : MonoBehaviour {
+	public Transform MCend;
+	void OnDisable() {
+		Instantiate(MCend);
+	}
+}
