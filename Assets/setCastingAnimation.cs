4b61e92de0b2b13da42b4570dedd543532bbf53a
EmilyClaudet <emily@kano.me>
2017-04-23 06:53:01 +0200

    Added opening casting animation and load to next scene

---
+++ Assets/setCastingAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setCastingAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Animator>().Play("casting");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
