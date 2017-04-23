6c2fdce488d53ae4d8d44717cc7f63fc1ab0a842
Catherine Claudet <cmclaudet@gmail.com>
2017-04-23 03:01:52 +0200

    Added brother happy text

---
+++ Assets/displayHappyText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class displayHappyText : MonoBehaviour {
+	public Transform happyText;
+	public string happyAnimationName;
+	// Use this for initialization
+	void Start () {
+		Transform text = Instantiate(happyText);
+		text.GetComponentInChildren<Animator>().Play(happyAnimationName);
+		text.GetComponentInChildren<displayText>().player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
