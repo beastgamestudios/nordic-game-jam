5bc8e964cfd1f70be9ab8187b6bc90a48373d050
Catherine Claudet <cmclaudet@gmail.com>
2017-04-24 18:19:41 +0200

    Added MC dialogue in main

---
+++ Assets/activateMCmain.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class activateMCmain : MonoBehaviour {
+	public GameObject MCmainText;
+	void OnDisable() {
+		MCmainText.SetActive(true);
+	}
+}
